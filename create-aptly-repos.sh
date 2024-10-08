f9f432aee4afe2e5f7115c33fc40d14294aef8e1
Jinna Kiisuo <jinnak@nocturnal.fi>
2024-10-09 01:38:45 +0300

    feat: data based publishing, still suffers from duplicates

--- create-aptly-repos.sh
+++ create-aptly-repos.sh
@@ -8,22 +8,41 @@ prefix="${2?}"
 readarray -t csv
 
 shopt -s extglob
+
+## Canonicalize csv input
+# Strip whitespace
 csv=( "${csv[@]/#+([[:blank:]])/}" ) 
 csv=( "${csv[@]/%+([[:blank:]])/}" ) 
 
-echo "# Repo config line examples" > "{$project}.list"
+# Drop unwanted lines
+for i in "${!csv[@]}"; do
+	if [[ "${csv[i]}" =~ ^#.* ]];then  # skip comments
+		unset 'csv[i]'
+		continue
+	fi
+	if [[ "${csv[i]}" =~ ^[[:space:]]*$ ]]; then # skip empty lines
+		unset 'csv[i]'
+		continue
+	fi
+done
 
-for reporaw in "${csv[@]}"; do
-	[[ "$reporaw" =~ ^#.* ]] && continue  # skip comments
-	[[ "$reporaw" =~ ^[[:space:]]*$ ]] && continue  # skip empty lines
 
-	distribution=$(echo "$reporaw" | xsv input -d \; | xsv select 1)
-	component=$(echo "$reporaw" | xsv input -d \; | xsv select 2)
-	archs=$(echo "$reporaw" | xsv input -d \; | xsv select 3 | tr -d \")
-	debglob=$(echo "$reporaw" | xsv input -d \; | xsv select 4)
+>&2 echo "Canonicalized input;"
+for repoline in "${csv[@]}"; do
+	echo "repodef: ${repoline}"
+done
+
+## Create repos
+>&2 echo "Creating repos:"
+echo "# Repo config line examples" > "${project}.list"
+for repoline in "${csv[@]}"; do
+	distribution=$(echo "$repoline" | xsv input -d \; | xsv select 1)
+	component=$(echo "$repoline" | xsv input -d \; | xsv select 2)
+	archs=$(echo "$repoline" | xsv input -d \; | xsv select 3 | tr -d \")
+	debglob=$(echo "$repoline" | xsv input -d \; | xsv select 4)
 	slug="${project}-${distribution}-${component}"
 
-	echo "deb [arch=${archs} signed-by=/etc/apt/trusted.gpg.d/${project}.gpg] https://repo.example.com ${distribution} ${component}" | tee -a "${project}.list"
+	echo "deb [arch=${archs} signed-by=/etc/apt/trusted.gpg.d/${project}.gpg] https://repo.example.com/${prefix} ${distribution} ${component}" | tee -a "${project}.list"
 	set -x
 	aptly repo create \
 		-distribution="$distribution" \
@@ -34,12 +53,23 @@ for reporaw in "${csv[@]}"; do
 	set +x
 done
 
-#TODO: All of this needs to detect data from the csv instead
-set +x
-for distribution in bookworm noble; do
+## Publish repos
+distros=($(printf -- '%s\n' "${csv[@]}" | xsv input -d \; | xsv select 1 | sort -u | xargs))
+>&2 echo "Publishing distros: ${distros[@]}"
+for distribution in ${distros[@]}; do
+	comps=($(printf -- '%s\n' "${csv[@]}" | xsv input -d \; | xsv search --select 1 "$distribution" | xsv select 2 | sort -u | xargs))
+	>&2 echo "Publishing for $distribution the following components: ${comps[@]}"
+	printf -v components '%s,' "${comps[@]}"
+	repos=()
+	for comp in "${comps[@]}"; do
+		repos+=("${project}-${distribution}-${comp}")
+	done
+
+	set -x
 	aptly publish repo \
-		-component=, \
+		-component="${components::-1}" \
 		-distribution="${distribution}" \
-		${project}-${distribution}-{stable,nightly} \
+		"${repos[@]}" \
 		"${prefix}"
+	set +x
 done
